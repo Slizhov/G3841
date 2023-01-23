5a91cf9ff1aa46e61aa2ef66accd11b44a599708
Slizhov Mikhail <slizhov383@gmail.com>
2023-01-23 22:19:08 +0300

    add solution for task 19,21,23

---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 21:
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+// * Сделать метод загрузки точек
+
+// Вариант 1:
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+// Выводим результат пользователю
+void PrintData(string msg, double res)
+{
+    Console.WriteLine(msg + res);
+}
+//Вычисляем расстояние между точками в 3D пространстве
+double CalcLen(int x1, int x2, int y1, int y2, int z1, int z2)
+{
+    double res = 0;
+    res = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+    return res;
+}
+
+//Вводим координаты точек
+int X1 = ReadData("Введите координату x1");
+int X2 = ReadData("Введите координату x2");
+int Y1 = ReadData("Введите координату y1");
+int Y2 = ReadData("Введите координату y2");
+int z1 = ReadData("Введите координату y2");
+int z2 = ReadData("Введите координату y2");
+
+//Вычисляем длину
+double len = CalcLen(X1, X2, Y1, Y2, z1, z2);
+
+//Выдаем результат
+PrintData("Расстояние между точками: ", len);
+
+// // Вариант 2:
+
+// int x1 = ReadInt("Введите координату X первой точки: ");
+// int y1 = ReadInt("Введите координату Y первой точки: ");
+// int z1 = ReadInt("Введите координату Z первой точки: ");
+// int x2 = ReadInt("Введите координату X второй точки: ");
+// int y2 = ReadInt("Введите координату Y второй точки: ");
+// int z2 = ReadInt("Введите координату Z второй точки: ");
+
+// int A = x2 - x1;
+// int B = y2 - y1;
+// int C = z1 - z2;
+
+// double length = Math.Sqrt(A * A + B * B + C * C);
+// Console.WriteLine($"Длинна отрезка {length}");
+
+
+// // Функция ввода сообщения
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
---
+++ Sem3Task23/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 23:
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+// * Вывести таблицу с границами и значениями друг над другом.
+
+// Вариант 1:
+// Ввод данных
+int num = ReadData("Введите N: ");
+
+//Собираем первую строчку таблицы
+string line1 = LineBuilder(num, 1);
+//Собираем вторую строчку таблицы
+string line2 = LineBuilder(num, 3);
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+//Выводим результат пользователю
+void PrintData(string msg1, string msg2)
+{
+    Console.WriteLine(msg1);
+    Console.WriteLine(msg2);
+}
+
+// Вывод нахождения степени чисел от 1 до N
+string LineBuilder(int n, int p)
+{
+    string s = "";
+    for (int i = 1; i <= n; i++)
+    {
+        s += Math.Pow(i, p).ToString() + "\t ";
+    }
+    return s;
+}
+// Вывод данных
+PrintData(line1,line2);
+
+
+
+// // Вариант 2:
+// int number = ReadInt("Введите число N: ");
+ 
+// for (int i = 1; i <= number; i++)
+// { 
+//     Console.Write($"{i*i*i} ");
+// }
+//  // Функция ввода сообщения
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
