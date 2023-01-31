75d000cb4c39ff8661e34c1b38845936428ccda3
Slizhov Mikhail <slizhov383@gmail.com>
2023-01-31 19:09:56 +0300

    add solution for task 34, 36, 38

---
+++ Sem5Task34/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 34: 
+// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] testArr = Gen1DArray(123,100,999);
+Print1DArr(testArr);
+int count = CountElem(testArr);
+PrintData("Количество четных чисел [100,999] :"+count);
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+int[] Gen1DArray(int len, int minValue, int maxValue)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+void Print1DArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+int CountElem(int[] arr)
+{
+    int res = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (Test(arr[i]))
+        {
+            res++;
+        }
+    }
+    return res;
+}
+
+bool Test(int n)
+{
+    return (n%2==0);
+}
+
---
+++ Sem5Task36/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 36: 
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] testArr = Gen1DArray(123,1,999);
+Print1DArr(testArr);
+int Sum = SumPos(testArr);
+PrintData("Сумма чисел стоящих на нечетных позициях :"+Sum);
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+int[] Gen1DArray(int len, int minValue, int maxValue)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+void Print1DArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+int SumPos(int[] arr)
+{
+    int res = 0;
+    for (int i = 1; i < arr.Length; i=i+2)
+    {
+        res+=arr[i];      
+    }
+    return res;
+}
